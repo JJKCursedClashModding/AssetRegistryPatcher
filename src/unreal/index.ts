501b3864dd6ad47a2a713a99fb3dcd0263381061
Ahmed <akelbann@gmail.com>
2026-04-21 21:42:12 +0800

    Get rid of packing/uassetgui pipeline

--- src/unreal/index.ts
+++ src/unreal/index.ts
@@ -15,11 +15,6 @@ export {
   type CookTagOptions,
 } from "./registry/saveRegistry.js";
 export {
-  loadedAssetDataFromStagingHints,
-  mergeAssetDataTags,
-} from "./registry/stagingAsset.js";
-export { verifyRegistryRoundTrip, type RoundTripResult } from "./registry/roundTrip.js";
-export {
   joinTopLevelPath,
   softObjectPathWireFromDisplayString,
   splitTopLevelAssetPath,
@@ -33,29 +28,3 @@ export {
   type ContentSegmentOptions,
 } from "./job/contentPath.js";
 export { parseStrictJson } from "./job/json.js";
-export {
-  assertJobShape,
-  assertJobRegistryBase,
-  assertRegistryRowBase,
-  resolvedContentMount,
-  resolvedContentOptions,
-  type FileToPackItem,
-  type JobFileV1,
-  type JobRegistryBase,
-  type JobReplaceEntry,
-  type JobSetFieldFn,
-  type RegistryRowBase,
-} from "./job/schema.js";
-export {
-  loadedAssetDataToRegistryBase,
-  resolveJobRegistryBase,
-} from "./registry/resolveRegistryBaseRef.js";
-export { expandFilesToPack, type ExpandedPackFile } from "./job/expandFilesToPack.js";
-export { runJob, type RunJobResult } from "./pipeline/runJob.js";
-export {
-  replaceAllGlobalLiteral,
-  replaceInString,
-  replaceInTagValues,
-  replaceInTagValuesEntry,
-} from "./pipeline/replace.js";
-export { setValueAtJsonPath } from "./pipeline/jsonPathSet.js";
